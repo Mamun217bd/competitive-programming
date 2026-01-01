882a0712de75531fdf0b51253ed5bc1ad08af39c
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-01 21:28:53 +0600

    Add Cricket World Cup Qualifier exercise solution

---
+++ problem-solving/CodeChef/difficulty-rating/500/Cricket_World_Cup_Qualifier.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x;
+    cin >> x;
+
+    if (x >= 12)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    return 0;
+}
