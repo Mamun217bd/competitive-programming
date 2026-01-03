f1ef5fe25431db8d0e458590740f1e52b6c4b985
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-03 22:26:37 +0600

    03-01-2026 Live Contest Task A Solution

---
+++ contests/AtCoder/Beginner-Contest-439/A_2_n_2_n.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << pow(2, n) - (2 * n) << endl;
+
+    return 0;
+}
