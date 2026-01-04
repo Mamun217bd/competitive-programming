774c471b688912dcc3eabb5d7f8ce982d885052e
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-04 23:28:39 +0600

    Add Clear Day exercise solution

---
+++ problem-solving/CodeChef/difficulty-rating/500/Clear_Day.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x, y;
+    cin >> x >> y;
+
+    cout << 7 - (x + y) << endl;
+
+    return 0;
+}
