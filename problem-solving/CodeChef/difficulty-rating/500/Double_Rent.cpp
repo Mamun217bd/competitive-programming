25a2e4641326e4e9ef0bf371d4ea98bd2b6854d4
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-05 17:46:15 +0600

    Add Double Rent exercise solution

---
+++ problem-solving/CodeChef/difficulty-rating/500/Double_Rent.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x;
+    cin >> x;
+
+    cout << x * 2 << endl;
+
+    return 0;
+}
