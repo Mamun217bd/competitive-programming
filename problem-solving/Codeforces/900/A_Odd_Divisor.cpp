0fc28d53b6c1ff3ad57fb6f611b18a927277b986
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-08 23:25:07 +0600

    1475A - Odd Divisor

---
+++ problem-solving/Codeforces/900/A_Odd_Divisor.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        while (n % 2 == 0)
+            n /= 2;
+
+        if (n > 1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
