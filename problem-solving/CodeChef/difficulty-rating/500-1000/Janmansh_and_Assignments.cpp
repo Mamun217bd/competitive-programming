a09874813ff00642cef2a7d7383c54e56ad0886c
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-11 21:59:54 +0600

    Add Janmansh and Assignments exercise solution
    https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/JASSIGNMENTS

---
+++ problem-solving/CodeChef/difficulty-rating/500-1000/Janmansh_and_Assignments.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x;
+        cin >> x;
+
+        10 - x >= 3 ? cout << "Yes" << endl : cout << "No" << endl;
+    }
+
+    return 0;
+}
