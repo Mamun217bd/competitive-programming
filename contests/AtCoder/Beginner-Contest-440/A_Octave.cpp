9580f95145d08d0a50a05377b755c82e0ff75709
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-11 22:18:29 +0600

    10-01-2026 Live Contest Task A Solution

---
+++ contests/AtCoder/Beginner-Contest-440/A_Octave.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x, y;
+    cin >> x >> y;
+
+    cout << x * pow(2, y) << endl;
+
+    return 0;
+}
