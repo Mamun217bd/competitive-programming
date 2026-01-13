cb4b71133be5b4eabef83c426359c1dda9850121
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-13 19:57:21 +0600

    Add CRED Coins exercise solution
    https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CREDCOINS

---
+++ problem-solving/CodeChef/difficulty-rating/500-1000/CRED_Coins.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        cout << (x * y) / 100 << endl;
+    }
+
+    return 0;
+}
