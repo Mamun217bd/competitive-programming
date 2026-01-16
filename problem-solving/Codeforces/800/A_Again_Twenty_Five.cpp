8948f0691c2cca6ade6ad94130a48f4675ee11bf
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-16 22:41:10 +0600

    630A - Again Twenty Five!

---
+++ problem-solving/Codeforces/800/A_Again_Twenty_Five.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+
+    cout << 25 << endl;
+
+    return 0;
+}
