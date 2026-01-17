0fb494a690edf9930365345dfdc06dd6347c0a06
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-17 22:48:43 +0600

    Add  404 Not Found exercise solution

---
+++ problem-solving/CodeChef/difficulty-rating/500/404_Not_Found.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x;
+    cin >> x;
+
+    x == 404 ? cout << "NOT FOUND" << endl : cout << "FOUND" << endl;
+
+    return 0;
+}
