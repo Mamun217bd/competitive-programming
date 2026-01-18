220219f535f523376ad4bde0b494e742f426f1c7
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-18 23:52:53 +0600

    Add Off By One exercise solution
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1

---
+++ problem-solving/CodeChef/difficulty-rating/500/Off_By_One.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << to_string(a + b) + '1' << endl;
+
+    return 0;
+}
