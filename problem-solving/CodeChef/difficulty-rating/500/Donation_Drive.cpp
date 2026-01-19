fa07265bf9a05e94e3effb636152fdd22b69d66a
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-19 23:18:25 +0600

    Add Donation Drive exercise solution
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DONDRIVE

---
+++ problem-solving/CodeChef/difficulty-rating/500/Donation_Drive.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, n;
+        cin >> x >> n;
+
+        cout << x - n << endl;
+    }
+
+    return 0;
+}
