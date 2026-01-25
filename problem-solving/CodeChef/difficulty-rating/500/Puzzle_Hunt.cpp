1c977f3b323d5b349674045c456bb70fde11c70b
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-25 22:11:13 +0600

    Add Puzzle Hunt exercise solution
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT

---
+++ problem-solving/CodeChef/difficulty-rating/500/Puzzle_Hunt.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if (n >= 6 && n <= 8)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    return 0;
+}
