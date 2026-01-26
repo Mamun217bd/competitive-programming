46d2b845ad8ac408feb693ef5228f37b6aa9486a
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-01-26 14:02:53 +0600

    Add Bone Appetit exercise solution

---
+++ problem-solving/CodeChef/difficulty-rating/500/Bone_Appetit.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    int x, y;
+    cin >> x >> y;
+
+    cout << (n * x) + (m * y) << endl;
+
+    return 0;
+}
