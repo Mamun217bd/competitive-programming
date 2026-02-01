174923094c6682c754d3f278d0dcc257ece44ea0
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-02-01 23:19:13 +0600

    112A - Petya and Strings

---
+++ problem-solving/Codeforces/800/A_Petya_and_Strings.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    for (int i = 0; i < s1.length(); i++)
+    {
+        s1[i] = tolower(s1[i]);
+        s2[i] = tolower(s2[i]);
+    }
+
+    if (s1 < s2)
+        cout << -1 << endl;
+    else if (s1 > s2)
+        cout << 1 << endl;
+    else
+        cout << 0 << endl;
+
+    return 0;
+}
