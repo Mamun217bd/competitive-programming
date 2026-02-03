38746cf938b40f9ad2859aeaac3714660086fc12
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-02-03 22:47:16 +0600

    266A - Stones on the Table

---
+++ problem-solving/Codeforces/800/A_Stones_on_the_Table.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int count = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (s[i] == s[i + 1])
+            count++;
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
