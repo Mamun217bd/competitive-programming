04ab7c7656e6658a8d740b459235b7f820e0b274
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-02-08 23:01:43 +0600

    Counting Words
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD

---
+++ problem-solving/CodeChef/difficulty-rating/500/Counting_Words.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        cout << (n * m) << endl;
+    }
+
+    return 0;
+}
