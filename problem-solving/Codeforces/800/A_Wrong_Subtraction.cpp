b4cd7246e081cba18f08367f0671e4277f3129f6
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-02-10 18:32:37 +0600

    977A - Wrong Subtraction

---
+++ problem-solving/Codeforces/800/A_Wrong_Subtraction.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < k; i++)
+    {
+        if (n % 10 == 0)
+            n /= 10;
+        else
+            n -= 1;
+    }
+
+    cout << n << endl;
+
+    return 0;
+}
