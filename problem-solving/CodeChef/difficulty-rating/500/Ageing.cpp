ea47527d8cca975318af6a74c639ef088c276590
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-02-11 17:52:08 +0600

    Ageing
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AGEING

---
+++ problem-solving/CodeChef/difficulty-rating/500/Ageing.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x;
+        cin >> x;
+
+        cout << x - 10 << endl;
+    }
+
+    return 0;
+}
