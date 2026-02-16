e3c3caedfb203a9b0639b072cbe9570e2bb75e6a
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-02-16 07:51:04 +0600

    Chairs Requirement
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHAIRS_

---
+++ problem-solving/CodeChef/difficulty-rating/500/Chairs_Requirement.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        if (x < y)
+            cout << 0 << endl;
+        else
+            cout << x - y << endl;
+    }
+
+    return 0;
+}
