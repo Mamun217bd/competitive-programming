f0352ce12f84b4bdffebf15ecc7bac189a2d59ac
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-02-23 05:47:28 +0600

    1722A - Spell Check

---
+++ problem-solving/Codeforces/800/A_Spell_Check.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        if (n != 5)
+        {
+            cout << "NO" << endl;
+            continue;
+        }
+
+        sort(s.begin(), s.end());
+
+        if (s == "Timru")
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
