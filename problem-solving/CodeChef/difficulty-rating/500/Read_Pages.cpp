81e19b0d2647f67ff6735ac667205da52c98b23f
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-03-01 22:59:36 +0600

    Read Pages
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/READPAGES

---
+++ problem-solving/CodeChef/difficulty-rating/500/Read_Pages.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, x, y;
+        cin >> n >> x >> y;
+
+        if (n > x * y)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+
+    return 0;
+}
