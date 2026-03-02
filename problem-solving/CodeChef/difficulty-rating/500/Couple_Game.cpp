5cd2ecc2ed8b31645622e8030e880291460d1951
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-03-02 06:17:20 +0600

    Couple Game
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/COUGAME

---
+++ problem-solving/CodeChef/difficulty-rating/500/Couple_Game.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int g, b;
+        cin >> g >> b;
+
+        cout << b - g << endl;
+    }
+
+    return 0;
+}
