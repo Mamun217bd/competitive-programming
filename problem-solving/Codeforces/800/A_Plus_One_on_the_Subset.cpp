25cd2d90582e97342ba11f638ab504250e758793
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-03-06 23:46:38 +0600

    1624A - Plus One on the Subset

---
+++ problem-solving/Codeforces/800/A_Plus_One_on_the_Subset.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        sort(a.begin(), a.end());
+
+        cout << a[n - 1] - a[0] << endl;
+    }
+
+    return 0;
+}
