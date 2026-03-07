c9a4035253301ed810b753868b8ec2771a5b91a0
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-03-07 23:40:01 +0600

    Chef and Chapters
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SEMCOURSES

---
+++ problem-solving/CodeChef/difficulty-rating/500/Chef_and_Chapters.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+
+        cout << x * y * z << endl;
+    }
+
+    return 0;
+}
