baf1a073648fae9bfe383d27cffc447f4c068a97
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-03-21 19:28:34 +0600

    The Gift
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CS2023_GIFT

---
+++ problem-solving/CodeChef/difficulty-rating/500/The_Gift.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x, n, m;
+    cin >> x >> n >> m;
+    
+    if(x + m >= n)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    
+    return 0;
+}
