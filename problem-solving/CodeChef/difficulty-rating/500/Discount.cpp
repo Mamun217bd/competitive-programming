ec34d51aabdca3f09f44d1d2a6bbee728045dfcc
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-03-26 23:06:11 +0600

    Discount
    https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DISCNT

---
+++ problem-solving/CodeChef/difficulty-rating/500/Discount.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int x;
+        cin >> x;
+        
+        cout << 100 - x << endl;
+    }
+    
+    return 0;
+}
