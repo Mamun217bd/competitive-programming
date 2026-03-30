6e4149bec7ed0a19832d091b46c058ea8e3555c7
Md Abdulllah Al Mamun <mamun217bd@gmail.com>
2026-03-30 23:19:10 +0600

    384A - Coder

---
+++ problem-solving/Codeforces/800/A_Coder.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if ((i + j) % 2 == 0)
+                count++;
+        }
+    }
+
+    cout << count << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if ((i + j) % 2 == 0)
+                cout << 'C';
+            else
+                cout << '.';
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
